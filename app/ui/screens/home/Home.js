9fec14e64f108166d9360e7d4e63565859ceb8b6
ezebarrena <ezebarrena@gmail.com>
2023-10-02 01:55:28 -0300

    NavBar Empezada

---
+++ app/ui/screens/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeUI from './HomeUI';
+import i18n from '../../../assets/strings/I18n';
+//funcion que crea pantalla 
+export default function Home() {
+    return (
+        <HomeUI />
+    )
+}
+
