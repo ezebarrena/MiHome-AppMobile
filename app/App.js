f86475e9ce2a81cc534776b2c93eecb654133f48
Agus Forti <agustin.forti@gmail.com>
2023-10-02 20:25:17 -0300

    CardPropiedades creada

--- app/App.js
+++ app/App.js
@@ -16,7 +16,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <CardPropiedad />
+      <Welcome />
     </View>
   );
 }
@@ -24,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
+
     alignItems: 'center',
     justifyContent: 'center',
   },
