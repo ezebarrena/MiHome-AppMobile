a703aa2bba1fc71cef1ae6ff118210254bb43f46
gnavarte <gnavarte@outlook.com>
2023-10-21 18:26:35 -0300

    +Modales

--- app/ui/components/modals/ConfirmationModal.jsx
+++ app/ui/components/modals/ConfirmationModal.jsx
@@ -1,1 +1,60 @@
+import React from "react";
+import { Modal, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
+const ConfirmationModal = ({ isVisible, message, onAccept, acceptColor = "#7cd9af", onDeny, denyColor = "#d97c7c" }) => {
+  return (
+    <Modal visible={isVisible} animationType="none" transparent={true}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.message}>{message}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={[styles.button, { backgroundColor: denyColor }]} onPress={onDeny}>
+              <Text style={styles.buttonText}>Denegar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, { backgroundColor: acceptColor }]} onPress={onAccept}>
+              <Text style={styles.buttonText}>Aceptar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    width: "80%",
+  },
+  message: {
+    marginHorizontal: 5,
+    marginVertical: 10,
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  button: {
+    flex: 1,
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default ConfirmationModal;
--- app/ui/components/modals/CustomAlert.jsx
+++ app/ui/components/modals/CustomAlert.jsx
@@ -1,44 +1,57 @@
 import React from "react";
-import { Modal, StyleSheet, Text, View, Pressable } from "react-native";
+import { Modal, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-const CustomAlert = ({ isVisible, onClose, title, message }) => {
-    return (
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={isVisible}
-        onRequestClose={onClose}
-        >
-        <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-            <View style={styles.closeButton}>
-                <Pressable onPress={onClose}>
-                <Text>X</Text>
-                </Pressable>
-            </View>
-            <Text>{title}</Text>
-            <Text>{message}</Text>
-            </View>
+const CustomAlert = ({ isVisible, onClose, message, buttonColor = "#7cd9af" }) => {
+  return (
+    <Modal visible={isVisible} animationType="none" transparent={true}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.message}>{message}</Text>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={[styles.button, { backgroundColor: buttonColor }]} onPress={onClose}>
+              <Text style={styles.buttonText}>Aceptar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        </Modal>
-    );
-    }
+      </View>
+    </Modal>
+  );
+};
 
 const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        justifyContent: "flex-end",
-    },
-    modalContent: {
-        backgroundColor: "#F7F2FA",
-        height: "60%",
-        borderTopLeftRadius: 40,
-        borderTopRightRadius: 40,
-        padding: 20,
-    },
-    closeButton: {
-        alignItems: "flex-end",
-    },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 20,
+    width: "80%",
+  },
+  message: {
+    marginHorizontal: 5,
+    marginVertical: 10,
+    alignSelf: "center",
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    margin: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+    width: "50%",
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
 });
 
 export default CustomAlert;
