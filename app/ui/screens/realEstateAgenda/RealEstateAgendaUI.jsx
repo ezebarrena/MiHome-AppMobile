a46b54ef1564beb64b8628485dbd82e6b2c87eb2
ezebarrena <ezebarrena@gmail.com>
2023-10-22 16:33:35 -0300

    NavBarRE nueva, falta acomodar boton +

--- app/ui/screens/realEstateAgenda/RealEstateAgendaUI.jsx
+++ app/ui/screens/realEstateAgenda/RealEstateAgendaUI.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet} from 'react-native'
+import Calendar from 'react-native-calendars/src/calendar';
 
 export default function RealEstateAgendaUI () {
   return (
-    <View>
-        
+    <View >
+      <View>
+        <Calendar style={styles.calendario} onDayPress={day => {console.log('selected day', day);}} markedDates={{
+        '2023-10-01': {selected: true, marked: true, selectedColor: 'blue'},
+        '2023-10-02': {selected: true, marked: true},
+        '2023-10-03': {selected: true, marked: true, selectedColor: 'blue'}
+        }}/>
+      </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  
+})
