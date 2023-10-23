2fb2f6485730ccdd78abae188875b23dc92d8504
gnavarte <gnavarte@outlook.com>
2023-10-23 19:08:46 -0300

    Corriemiento appjs

--- app/App.js
+++ app/App.js
@@ -4,18 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import * as Localization from 'expo-localization';
 import { I18n } from 'i18n-js'
-import { translations } from './localization';
-import UserProfile from './app/ui/screens/userProfile/UserProfile.js'
-import RealEstateProfile from './app/ui/screens/realEstateProfile/RealEstateProfile'
+import { translations } from '../localization';
+import UserProfile from './ui/screens/userProfile/UserProfile.js'
+import RealEstateProfile from './ui/screens/realEstateProfile/RealEstateProfile'
 
-import CardPropiedad from './app/ui/components/cardPropiedad/cardPropiedad';
+import CardPropiedad from './ui/components/cardPropiedad/cardPropiedad';
 
 
 import React from "react";
 
 import { NavigationContainer } from '@react-navigation/native';
-import { MainStackNavigator } from './app/navigation/MainStackNavigator';
-import { LandingStackNavigator } from './app/navigation/LandingStackNavigator'
+import { MainStackNavigator } from './navigation/MainStackNavigator';
+import { LandingStackNavigator } from './navigation/LandingStackNavigator'
 
 export default function App() {
   return (
