bc8ee19188840d4385faf1213e27d4a2ed91ff1f
gnavarte <gnavarte@outlook.com>
2023-10-30 21:27:03 -0300

    ImagePickerModal

--- app/ui/screens/realEstateUploadAsset/uploadAssetUI.jsx
+++ app/ui/screens/realEstateUploadAsset/uploadAssetUI.jsx
@@ -1,15 +1,5 @@
-import React from "react";
-import {
-    ImageBackground,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    Pressable,
-    Image,
-    Touchable,
-    TouchableOpacity,
-} from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import i18n from "../../../assets/strings/I18n";
 import Theme from "../../styles/Theme";
 import { Dimensions } from "react-native";
@@ -17,256 +7,233 @@ import CustomTextInput from "../../../ui/components/inputs/CustomTextInput";
 import ChoiceInput from "../../../ui/components/inputs/ChoiceInput";
 import ChoiceMultipleInput from "../../../ui/components/inputs/ChoiceMultipleInput";
 import Button from "../../../ui/components/buttons/Button";
-import {launchImageLibrary} from 'react-native-image-picker';
 import MapScreen from "../../components/cards/CardMap";
-
-//import ImagePicker2 from 'react-native-image-crop-picker';
-/*
-const selectImageFromGallery = () => {
-    ImagePicker2.openPicker({
-      width: 300,
-      height: 400,
-      cropping: true,
-    }).then(image => {
-      console.log(image);
-    }).catch(error => {
-      console.log(error);
-    });
-  };
-*/
-const ImagePicker = () => {
-    let options = {
-        storageOptions: {
-            path: 'image',
-        },
-    };
-
-    launchImageLibrary(options, response => {
-        console.log(response);
-    })
-}
-
+import ImagePickerModal from "../../components/modals/ImagePickerModal";
 
 const dataTypes = [
-    {key:'1', value:'Casa'},
-    {key:'2', value:'Departamento'},
-    {key:'3', value:'Casa quinta'},
-    {key:'4', value:'PH'},
-    {key:'5', value:'Galpon'},
-    {key:'5', value:'Oficina'},
-    {key:'5', value:'Comercion'},
-    {key:'5', value:'Terreno'},
-]
+    { key: '1', value: 'Casa' },
+    { key: '2', value: 'Departamento' },
+    { key: '3', value: 'Casa quinta' },
+    { key: '4', value: 'PH' },
+    { key: '5', value: 'Galpon' },
+    { key: '6', value: 'Oficina' },
+    { key: '7', value: 'Comercio' },
+    { key: '8', value: 'Terreno' },
+];
 const dataTransaccion = [
-    {key:'1', value:'Venta'},
-    {key:'2', value:'Alquiler'},
-]
+    { key: '1', value: 'Venta' },
+    { key: '2', value: 'Alquiler' },
+];
 const dataCurrency = [
-    {key:'1', value:'U$D dolar estadounidense'},
-    {key:'2', value:'AR$ peso argentino'},
-]
+    { key: '1', value: 'U$D dolar estadounidense' },
+    { key: '2', value: 'AR$ peso argentino' },
+];
 const dataRooms = [
-    {key:'1', value:'1'},
-    {key:'2', value:'2'},
-    {key:'3', value:'3'},
-    {key:'4', value:'4'},
-    {key:'5', value:'5'},
-    {key:'6', value:'6'},
-    {key:'7', value:'7'},
-    {key:'8', value:'8'},
-    {key:'9', value:'9'},
-    {key:'10', value:'10'},
-    {key:'11', value:'Mas de 10'},
-]
+    { key: '1', value: '1' },
+    { key: '2', value: '2' },
+    { key: '3', value: '3' },
+    { key: '4', value: '4' },
+    { key: '5', value: '5' },
+    { key: '6', value: '6' },
+    { key: '7', value: '7' },
+    { key: '8', value: '8' },
+    { key: '9', value: '9' },
+    { key: '10', value: '10' },
+    { key: '11', value: 'Mas de 10' },
+];
 const dataFloors = [
-    {key:'1', value:'1'},
-    {key:'2', value:'2'},
-    {key:'3', value:'3'},
-    {key:'4', value:'4 o mas'},
-]
+    { key: '1', value: '1' },
+    { key: '2', value: '2' },
+    { key: '3', value: '3' },
+    { key: '4', value: '4 o mas' },
+];
 const dataBaths = [
-    {key:'1', value:'1'},
-    {key:'2', value:'2'},
-    {key:'3', value:'3'},
-    {key:'4', value:'4 o mas'},
-]
+    { key: '1', value: '1' },
+    { key: '2', value: '2' },
+    { key: '3', value: '3' },
+    { key: '4', value: '4 o mas' },
+];
 const dataBedrooms = [
-    {key:'1', value:'1'},
-    {key:'2', value:'2'},
-    {key:'3', value:'3'},
-    {key:'4', value:'4 o mas'},
-]
+    { key: '1', value: '1' },
+    { key: '2', value: '2' },
+    { key: '3', value: '3' },
+    { key: '4', value: '4 o mas' },
+];
 const dataGarages = [
-    {key:'1', value:'1 vehiculo'},
-    {key:'2', value:'2 vehiculos'},
-    {key:'3', value:'3 vehiculos'},
-    {key:'4', value:'4 o mas vehiculos'}
-]
+    { key: '1', value: '1 vehiculo' },
+    { key: '2', value: '2 vehiculos' },
+    { key: '3', value: '3 vehiculos' },
+    { key: '4', value: '4 o mas vehiculos' },
+];
 const dataAmenities = [
-    {key:'1', value:'pileta'},
-    {key:'2', value:'pileta climatizada'},
-    {key:'3', value:'pileta techada'},
-    {key:'4', value:'jacuzzi'},
-    {key:'5', value:'Gimnasio'},
-    {key:'6', value:'Quincho'},
-    {key:'7', value:'Parrilla'},
-    {key:'8', value:'Terraza'},
-    {key:'9', value:'Jardin'},
-    {key:'10', value:'Seguridad privada'},
-    {key:'11', value:'Espacio para deportes'},
-]
-
+    { key: '1', value: 'pileta' },
+    { key: '2', value: 'pileta climatizada' },
+    { key: '3', value: 'pileta techada' },
+    { key: '4', value: 'jacuzzi' },
+    { key: '5', value: 'Gimnasio' },
+    { key: '6', value: 'Quincho' },
+    { key: '7', value: 'Parrilla' },
+    { key: '8', value: 'Terraza' },
+    { key: '9', value: 'Jardin' },
+    { key: '10', value: 'Seguridad privada' },
+    { key: '11', value: 'Espacio para deportes' },
+];
 
 export default function UploadAssetUI() {
+    const [imageUri, setImageUri] = useState(null);
+
+    const renderImagePreview = (imageUri) => {
+        if (imageUri) {
+            return (
+                <View>
+                    <Image source={{ uri: imageUri }} style={styles.previewImage} />
+                </View>
+            );
+        }
+        return null;
+    };
 
     return (
-        <ScrollView style = {styles.ScrollView}>
-
+        <ScrollView style={styles.ScrollView}>
             <View style={styles.contenedorHead}>
                 <Text style={styles.textoHead}>{i18n.t('realEstateUploadAsset.headTitle')}</Text>
             </View>
-            
-            <View style={styles.dataEntry}> 
-            
+
+            <View style={styles.dataEntry}>
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.title')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.image')}</Text>
-                <TouchableOpacity style={styles.test}
-                    onPress={() => {
-                        ImagePicker();
-                        //selectImageFromGallery();
-                    }}
-                >
-                    <Text style={styles.textoBody2}>Cargar Imagen desde galeria</Text>
-                </TouchableOpacity>
+                <ImagePickerModal
+                    buttonText="Seleccionar imagen"
+                    modalTitle="Seleccionar imagen"
+                    onImageSelected={(uri) => setImageUri(uri)}
+                />
+                {renderImagePreview(imageUri)}
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.type')}</Text>
-                <ChoiceInput data={dataTypes}/>
+                <ChoiceInput data={dataTypes} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.transaction')}</Text>
-                <ChoiceInput data={dataTransaccion}/>
+                <ChoiceInput data={dataTransaccion} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.price')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.coin')}</Text>
-                <ChoiceInput data={dataCurrency}/>
+                <ChoiceInput data={dataCurrency} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.bills')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.description')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.amenities')}</Text>
-                <ChoiceMultipleInput data={dataAmenities}/>
+                <ChoiceMultipleInput data={dataAmenities} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.rooms')}</Text>
-                <ChoiceInput data={dataRooms}/>
+                <ChoiceInput data={dataRooms} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.floors')}</Text>
-                <ChoiceInput data={dataFloors}/>
+                <ChoiceInput data={dataFloors} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.bath')}</Text>
-                <ChoiceInput data={dataBaths}/>
+                <ChoiceInput data={dataBaths} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.bedroom')}</Text>
-                <ChoiceInput data={dataBedrooms}/>
+                <ChoiceInput data={dataBedrooms} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.garage')}</Text>
-                <ChoiceInput data={dataGarages}/>
+                <ChoiceInput data={dataGarages} />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.mTotal')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
-                <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.mCover')} </Text>
-                <CustomTextInput/>
+                <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.mCover')}</Text>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.storage')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.antiquity')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.frontBack')}</Text>
-                <CustomTextInput/>
+                <CustomTextInput />
 
                 <Text style={styles.textoBody1}>{i18n.t('realEstateUploadAsset.location')}</Text>
-                <CustomTextInput/>
-                <MapScreen/>
+                <CustomTextInput />
+                <MapScreen />
 
-
-                <Button title={"Publicar"} titleColor={"white"}/>
-
+                <Button title={"Publicar"} titleColor={"white"} />
             </View>
-
         </ScrollView>
     );
 }
-    
 
-    const styles = StyleSheet.create({
-        ScrollView: {
-            flex: 1,
-            width: "100%",
-            height: "100%",
-        },
-
-        background: {
-            position: "absolute",
-            left: 0,
-            right: 0,
-            height: "100%",
-        },
-
-        inputImagen:{
-            width: 200, 
-            height: 200 
-        },
-
-        textoHead: {
-            marginTop: 20,
-            fontFamily: 'Poppins_700Bold',
-            color: 'black',
-            fontSize: Dimensions.get('window').width * 0.07,
-        },
-
-        contenedorHead: {
-            flexDirection: 'row', // Coloca los elementos uno al lado del otro horizontalmente
-            alignItems: 'center',
-            marginLeft: "3%",
-            paddingTop: 30,
-          },
-        
-        textoBody1: {
-            fontFamily: "Poppins_500Medium", //cambiar a roboto
-            fontSize: Dimensions.get('window').width * 0.045,
-            marginLeft: "3%",
-          },
-
-        textoBody2: {
-            fontFamily: "Poppins_500Medium", //cambiar a roboto
-            color: 'white',
-            textAlign: 'center',
-            fontSize: Dimensions.get('window').width * 0.035,
-            marginLeft: "3%",
-        },
-
-        test: {
-            height:50,
-            backgroundColor: Theme.colors.PRIMARY,
-            marginTop:10,
-            marginBottom:10,
-            borderRadius: 10,
-            borderWidth: 1,
-            margin: 14,
-            justifyContent: 'center',
-            alignItems: 'center',
-
-        }
-        
-
+const styles = StyleSheet.create({
+    ScrollView: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
     },
 
-);
+    background: {
+        position: "absolute",
+        left: 0,
+        right: 0,
+        height: "100%",
+    },
+
+    inputImagen: {
+        width: 200,
+        height: 200,
+    },
+
+    textoHead: {
+        marginTop: 20,
+        fontFamily: 'Poppins_700Bold',
+        color: 'black',
+        fontSize: Dimensions.get('window').width * 0.07,
+    },
+
+    contenedorHead: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: "3%",
+        paddingTop: 30,
+    },
+
+    textoBody1: {
+        fontFamily: "Poppins_500Medium",
+        fontSize: Dimensions.get('window').width * 0.045,
+        marginLeft: "3%",
+    },
+
+    textoBody2: {
+        fontFamily: "Poppins_500Medium",
+        color: 'white',
+        textAlign: 'center',
+        fontSize: Dimensions.get('window').width * 0.035,
+        marginLeft: "3%",
+    },
+
+    test: {
+        height: 50,
+        backgroundColor: Theme.colors.PRIMARY,
+        marginTop: 10,
+        marginBottom: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        margin: 14,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    previewImage: {
+        width: "100%",
+        aspectRatio: 16 / 9,
+        borderRadius: 5,
+    },
+});
