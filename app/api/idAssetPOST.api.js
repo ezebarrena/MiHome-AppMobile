b5dffd49b4626fbb7e70dc2bf8bc3187c6a4a142
gnavarte <gnavarte@outlook.com>
2023-11-05 17:24:15 -0300

    Refactor code for better performance and readability.

---
+++ app/api/idAssetPOST.api.js
@@ -0,0 +1,25 @@
+const postIdAsset = async (idPropiedad) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({
+        "_id": idPropiedad,
+    });
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        //redirect: 'follow',
+        //mode: 'cors',
+    };
+    
+    let response = await fetch("http://192.168.1.6:8080/idAssets", requestOptions)
+    let jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData.asset;
+
+
+}
+
+export default postIdAsset;
