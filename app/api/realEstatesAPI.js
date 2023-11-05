494532dcc69edd902b31aa8cb0591e25b0b785a7
gnavarte <gnavarte@outlook.com>
2023-11-05 16:14:22 -0300

    login

--- app/api/realEstatesAPI.js
+++ app/api/realEstatesAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://tu-direccion-del-backend.com'; // Reemplaza con la URL de tu backend
+const API_BASE_URL = 'http://3.17.181.224:8080'; // Reemplaza con la URL de tu backend
 
 // Crea una instancia personalizada de Axios
 const realEstatesAPI = axios.create({
@@ -23,7 +23,7 @@ export const signInRealEstate = async (realEstateData) => {
 
 export const logInRealEstate = async (realEstateData) => {
   try {
-    const response = await realEstatesAPI.post('/auths/l', realEstateData);
+    const response = await realEstatesAPI.post('/auths/login', realEstateData);
     return response.data;
   } catch (error) {
     console.error('Error logging in real estate:', error);
---
+++ app/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useForm = (initState) => {
+    
+    const [state, setState] = useState(initState);
+
+    const onChange = (value, field) => {
+        setState({
+            ...state,
+            [field]: value
+        });
+    }
+
+    return {
+        ...state,
+        form: state,
+        onChange,
+    }
+}
