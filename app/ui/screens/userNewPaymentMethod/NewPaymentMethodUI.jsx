97b94ffd826e33bc1c47cbb317fea6663b3eee62
gnavarte <gnavarte@outlook.com>
2023-12-02 13:35:17 -0300

    Refactor input field limits and add navigation

--- app/ui/screens/userNewPaymentMethod/NewPaymentMethodUI.jsx
+++ app/ui/screens/userNewPaymentMethod/NewPaymentMethodUI.jsx
@@ -17,51 +17,70 @@ const NewPaymentMethodUI = () => {
         cardCvv: "",
         cardBank: "",
     });
-    
+
+    const limitInput = (value, fieldName, maxLength) => {
+        const truncatedValue = value.slice(0, maxLength);
+        onChange(truncatedValue, fieldName);
+    };
+
+    const limitExpirationInput = (value, fieldName) => {
+        const numericValue = value.replace(/\D/g, '').slice(0, 4);
+
+        if (numericValue.length === 3) {
+            const formattedValue = numericValue.slice(0, 2) + '/' + numericValue.slice(2);
+            onChange(formattedValue, fieldName);
+        } else if (numericValue.length === 4) {
+            const formattedValue = numericValue.replace(/^(\d{2})/, '$1/');
+            onChange(formattedValue, fieldName);
+        } else {
+            onChange(numericValue, fieldName);
+        }
+    };
+
     return (
         <View style={styles.container}>
-        <CreditCard
-            cardNumber={form.cardNumber}
-            cardHolder={form.cardHolder}
-            cardExpiration={form.cardExpiration}
-            cardCvv={form.cardCvv}
-            cardBank={form.cardBank}
-        />
-        <CustomTextInput
-            placeholder="Número de tarjeta"
-            onChangeText={(value) => onChange(value, "cardNumber")}
-            value={form.cardNumber}
-            keyboardType="numeric"
-        />
-        <CustomTextInput
-            placeholder="Titular"
-            onChangeText={(value) => onChange(value, "cardHolder")}
-            value={form.cardHolder}
-        />
-        <CustomTextInput
-            placeholder="Vencimiento"
-            onChangeText={(value) => onChange(value, "cardExpiration")}
-            value={form.cardExpiration}
-            keyboardType="numeric"
-        />
-        <CustomTextInput
-            placeholder="CVV"
-            onChangeText={(value) => onChange(value, "cardCvv")}
-            value={form.cardCvv}
-            keyboardType="numeric"
-        />
-        <ChoiceInput
-            data={[
-                { value: "SANTANDER" },
-                { value: "GALICIA" },
-                { value: "BBVA" },
-            ]}
-            onValueSelect={(value) => onChange(value, "cardBank")}
-        />
-        <Button
-            title="Confirmar"
-            onPress={() => navigation.navigate("UserPaymentMethods")}
-        />
+            <CreditCard
+                cardNumber={form.cardNumber}
+                cardHolder={form.cardHolder}
+                cardExpiration={form.cardExpiration}
+                cardCvv={form.cardCvv}
+                cardBank={form.cardBank}
+            />
+            <CustomTextInput
+                placeholder="Número de tarjeta"
+                onChangeText={(value) => limitInput(value, "cardNumber", 16)}
+                value={form.cardNumber}
+                keyboardType="numeric"
+            />
+            <CustomTextInput
+                placeholder="Titular"
+                onChangeText={(value) => limitInput(value, "cardHolder", 20)}
+                value={form.cardHolder}
+            />
+            <CustomTextInput
+                placeholder="Vencimiento"
+                onChangeText={(value) => limitExpirationInput(value, "cardExpiration")}
+                value={form.cardExpiration}
+                keyboardType="numeric"
+            />
+            <CustomTextInput
+                placeholder="CVV"
+                onChangeText={(value) => limitInput(value, "cardCvv", 3)}
+                value={form.cardCvv}
+                keyboardType="numeric"
+            />
+            <ChoiceInput
+                data={[
+                    { value: "SANTANDER" },
+                    { value: "GALICIA" },
+                    { value: "BBVA" },
+                ]}
+                onValueSelect={(value) => onChange(value, "cardBank")}
+            />
+            <Button
+                title="Confirmar"
+                onPress={() => navigation.navigate("UserPaymentMethods")}
+            />
         </View>
     );
 }
