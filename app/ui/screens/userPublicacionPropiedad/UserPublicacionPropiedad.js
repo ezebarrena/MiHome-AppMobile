6712c0b9c5ccc76a24151678332b650748c3dbf0
Agus Forti <agustin.forti@gmail.com>
2023-12-04 15:21:15 -0300

    Otros cambios

--- app/ui/screens/userPublicacionPropiedad/UserPublicacionPropiedad.js
+++ app/ui/screens/userPublicacionPropiedad/UserPublicacionPropiedad.js
@@ -43,7 +43,7 @@ export default function PublicacionPropiedad({ route }) {
     return (
         <React.Fragment>
             {informacion && inmobiliaria ? (
-                <PublicacionPropiedadUI propiedad={informacion} inmobiliaria={inmobiliaria} />
+                <UserPublicacionPropiedadUI propiedad={informacion} inmobiliaria={inmobiliaria} />
             ) : (
                 <ActivityIndicator style={{flex:1}} size="large" />
             )}
