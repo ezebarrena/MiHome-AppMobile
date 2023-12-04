69c68c90d9db64c7a2b95fe49c614b1380e3873c
Agus Forti <agustin.forti@gmail.com>
2023-12-04 16:55:30 -0300

    busqueda

--- app/ui/screens/userSearchResult/SearchResults.js
+++ app/ui/screens/userSearchResult/SearchResults.js
@@ -2,7 +2,9 @@ import React from 'react';
 import SearchResultsUI from './SearchResultsUI';
 import i18n from '../../../assets/strings/I18n';
 //funcion que crea pantalla 
-export default function SearchResults() {
+export default function SearchResults({route}) {
+    const { transaction, type  } = route.params;
+    console.log(transaction, type);
     return (
         <SearchResultsUI />
     )
