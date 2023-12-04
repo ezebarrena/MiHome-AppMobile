e705c50eba1e4bb601ecd05673f190126fba28e0
gnavarte <gnavarte@outlook.com>
2023-12-04 17:21:49 -0300

    Add payment method deletion functionality

--- app/ui/screens/userPaymentMethods/PaymentMethodsUI.jsx
+++ app/ui/screens/userPaymentMethods/PaymentMethodsUI.jsx
@@ -11,6 +11,8 @@ const PaymentMethodsUI = () => {
     const navigation = useNavigation();
     const [isConfirmationModalVisible, setIsConfirmationModalVisible] = React.useState(false);
     const [paymentMethods, setPaymentMethods] = useState([]);
+    const [paymentMethodtoDelete, setPaymentMethodToDelete] = useState(null);
+    const [isPaymentMethodDeleted, setIsPaymentMethodDeleted] = useState(false);
 
     useEffect(() => {
         const getPaymentMethodsFromAPI = async () => {
@@ -20,90 +22,22 @@ const PaymentMethodsUI = () => {
             setPaymentMethods(paymentMethodsFromAPI);
         };
         getPaymentMethodsFromAPI();
-    }, []);
-    
-    // const paymentMethods = [
-    //     {
-    //         id: 1,
-    //         cardNumber: "5555555555555555",
-    //         cardExpiration: "12/25",
-    //         cardBank: "SANTANDER",
-    //     },
-    //     {
-    //         id: 2,
-    //         cardNumber: "4444444444444444",
-    //         cardExpiration: "12/25",
-    //         cardBank: "GALICIA",
-    //     },
-    //     {
-    //         id: 3,
-    //         cardNumber: "5555333333333333",
-    //         cardExpiration: "12/25",
-    //         cardBank: "BBVA",
-    //     },
-    //     {
-    //         id: 4,
-    //         cardNumber: "5555555555555555",
-    //         cardExpiration: "12/25",
-    //         cardBank: "SANTANDER",
-    //     },
-    //     {
-    //         id: 5,
-    //         cardNumber: "4444444444444444",
-    //         cardExpiration: "12/25",
-    //         cardBank: "GALICIA",
-    //     },
-    //     {
-    //         id: 6,
-    //         cardNumber: "5555333333333333",
-    //         cardExpiration: "12/25",
-    //         cardBank: "BBVA",
-    //     },
-    //     {
-    //         id: 7,
-    //         cardNumber: "5555555555555555",
-    //         cardExpiration: "12/25",
-    //         cardBank: "SANTANDER",
-    //     },
-    //     {
-    //         id: 8,
-    //         cardNumber: "4444444444444444",
-    //         cardExpiration: "12/25",
-    //         cardBank: "GALICIA",
-    //     },
-    //     {
-    //         id: 9,
-    //         cardNumber: "5555333333333333",
-    //         cardExpiration: "12/25",
-    //         cardBank: "BBVA",
-    //     },
-    //     {
-    //         id: 10,
-    //         cardNumber: "5555555555555555",
-    //         cardExpiration: "12/25",
-    //         cardBank: "SANTANDER",
-    //     },
-    //     {
-    //         id: 11,
-    //         cardNumber: "4444444444444444",
-    //         cardExpiration: "12/25",
-    //         cardBank: "GALICIA",
-    //     },
-    //     {
-    //         id: 12,
-    //         cardNumber: "5555333333333333",
-    //         cardExpiration: "12/25",
-    //         cardBank: "BBVA",
-    //     },
-    // ];
+    }, [isPaymentMethodDeleted]);   
 
-    const onPressTrashIcon = (id) => {
-        console.log("Trash icon pressed for card with id: ", id);
+    const onPressTrashIcon = (paymentMethod) => {
+        console.log("Trash icon pressed for card with id: ", paymentMethod);
+        setPaymentMethodToDelete(paymentMethod);
         setIsConfirmationModalVisible(true);
     };
 
     const onAccept = () => {
-        console.log("Accepted");
+        const deletePaymentMethodFromAPI = async () => {
+            const userId = await AsyncStorage.getItem('userId')
+            const response = await deletePaymentMethod(userId, paymentMethodtoDelete);
+            console.log("Response from API: ", response);
+            setIsPaymentMethodDeleted(response);
+        };
+        deletePaymentMethodFromAPI();
         setIsConfirmationModalVisible(false);
     };
 
@@ -118,10 +52,11 @@ const PaymentMethodsUI = () => {
                 {paymentMethods && paymentMethods.map((paymentMethod) => (
                     <CreditCardPreview
                         key={paymentMethod._id}
-                        cardNumber={paymentMethod.cardNumber}
+                        cardNumber={paymentMethod.cardNumber.toString()}
                         cardExpiration={paymentMethod.expiration}
                         cardBank={paymentMethod.bank}
-                        onPressTrashIcon={() => onPressTrashIcon(paymentMethod._id)}
+                        showTrashIcon={true}
+                        onPressTrashIcon={() => onPressTrashIcon(paymentMethod)}
                     />
                 ))}
             </ScrollView>
